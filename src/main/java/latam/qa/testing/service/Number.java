d79b387071888ec902b865352ca43c11aec3a521
root <root@localhost.localdomain>
2016-12-03 14:43:21 -0300

    Segundo ejercicio Terminado

--- src/main/java/latam/qa/testing/service/Number.java
+++ src/main/java/latam/qa/testing/service/Number.java
@@ -5,4 +5,15 @@ public class Number {
     public String stringCalc(int x, int y){
     	return String.format("%d,%d",x,y);
     }
+
+   public int divideNumeros(int a, int b){
+	if(b==0){
+return 0;
+}else{
+int retorno = a/b;
+return retorno;
+}
+	
+
+}
 }
--- src/test/java/latam/qa/testing/service/NumberTest.java
+++ src/test/java/latam/qa/testing/service/NumberTest.java
@@ -20,4 +20,25 @@ public class NumberTest {
         assertNotSame(String.format("%d,%d", 1, 5), result);
 
     }
+
+    @Test
+ public void divideCero(){
+	Number n = new Number();
+	int division = n.divideNumeros(1,0);
+	if(division==0){
+	System.out.println("No se pueden realizar divisiones por 0");
+}
+}
+
+	@Test
+public void numeroConocido(){
+
+	Number n = new Number();
+	int division1=n.divideNumeros(4,2);
+	int division2=n.divideNumeros(9,2);
+	System.out.println("Division 1="+division1);
+	System.out.println("Division 2="+division2);
+}
+
+
 }
